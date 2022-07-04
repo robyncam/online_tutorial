49d8ce2a4ef4cae2b93fefc534bd73185ceb9e07
robyncam <robyncameron7@gmail.com>
2022-07-04 19:38:07 +0000

    added a free HTML template from online and changed the index.html file to load in django

--- mysite/views.py
+++ mysite/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 
 def index(request):
-    return render(request, 'index.html')
+    feature1 = Feature()
+    feature1.id = 0
+    feature1.name = 'Fast'
+    feature1.details = 'Our service is very quick'
+    return render(request, 'index.html', {'feature': feature1})
 
 def counter(request):
     text = request.POST['text']
