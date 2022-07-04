c152ec4092245c0a8ef0939f08baa6c81aa3ec3d
robyncam <robyncameron7@gmail.com>
2022-07-04 20:00:23 +0000

    added a for loop in the html to loop through the different features to print the boxes on the page for fast, reliable, etc. created a list called features which contains the information to be looped though, in models created a class called Feature

--- mysite/views.py
+++ mysite/views.py
@@ -8,7 +8,25 @@ def index(request):
     feature1.id = 0
     feature1.name = 'Fast'
     feature1.details = 'Our service is very quick'
-    return render(request, 'index.html', {'feature': feature1})
+
+    feature2 = Feature()
+    feature2.id = 1
+    feature2.name = 'Reliable'
+    feature2.details = 'Our service is very reliable'
+
+    feature3 = Feature()
+    feature3.id = 2
+    feature3.name = 'Easy to Use'
+    feature3.details = 'Our service is very easy to use'
+
+    feature4 = Feature()
+    feature4.id = 3
+    feature4.name = 'Affordable'
+    feature4.details = 'Our service is very affordable'
+
+    features = [feature1, feature2, feature3, feature4]
+
+    return render(request, 'index.html', {'features': features})
 
 def counter(request):
     text = request.POST['text']
