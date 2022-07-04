512e8d1086b0cc765cfb143d6b429a8b27fd23ad
robyncam <robyncameron7@gmail.com>
2022-07-04 17:35:14 +0000

    changed the reques to a POST request and added a csrf token

--- mysite/views.py
+++ mysite/views.py
@@ -6,7 +6,7 @@ def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     word_count = len(text.split())
     return render(request, 'counter.html', {'word_count': word_count})
 
